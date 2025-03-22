02c7800f67f599459a26d2a88e26409381453c84
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-03-23 03:29:10 +0700

    exp

--- frontend/app.py
+++ frontend/app.py
@@ -75,4 +75,4 @@ def index():
     return render_template('index.html', bar_chart=bar_chart, pie_chart=pie_chart, heatmap_chart=heatmap_chart)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run()
